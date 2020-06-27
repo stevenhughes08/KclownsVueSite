9db74207b70db1c68cf1cb0918deffe21928e0f9
Steven <stevenhughes08@gmail.com>
2020-06-27 14:53:16 -0400

    Created Our story and Characters vue files

---
+++ website/src/components/Characters.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Characters</h1>
+        <p>This is characters text.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Characters'
+}
+</script>
+
+<style lang="scss">
+
+</style>
